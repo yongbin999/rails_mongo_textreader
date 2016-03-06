e3b4c98131b937bc85fe19b8dfee6b853623e255
yongbin999 <yongbinliang@umass.edu>
2016-03-06 14:44:31 -0500

    finish text return, update proj status

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -1,22 +1,15 @@
 class LinesController < ApplicationController
 	def index
 	end
-	def newfile
-		@file = 1
-			if request.xhr?
-		      render 'newfile.html'
-		    else
-		      respond_to do |format|
-		        format.html { render :action => 'newfile', :layout => false }
-		      end
-		    end
-	end
+
 	def get
 		@line = params[:line]
-		@new_files = NewFile.where(:fileid => 1, :line => @line )
+		@content = NewFile.where(:fileid => 1, :line => @line)[0].content
+
+		puts @content
 		
-		if @new_files.length != 0
-		      render json: @new_files , :status => :ok
+		if @content
+		      render text: @content, :status => :ok
 		    else
 		      render :file => 'public/413.html', :status => :request_entity_too_large, :layout => false
 		end
