b7a470e13094f952473675603911fb481224fa6a
yongbin999 <yongbinliang@umass.edu>
2016-03-05 22:01:49 -0500

    add upload view, mongo setup

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -2,7 +2,14 @@ class LinesController < ApplicationController
 	def index
 	end	
 	def newfile
-		 render plain: "to be added with upload plugins"
+		@file = 1
+			if request.xhr?
+		      render 'newfile.html'
+		    else
+		      respond_to do |format|
+		        format.html { render :action => 'newfile', :layout => false }
+		      end
+		    end
 	end
 	def get
 		 render plain: params[:line].inspect
