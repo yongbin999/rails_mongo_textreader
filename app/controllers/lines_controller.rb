63375c8e834c1eeded325e54e9404f30246a5d38
yongbin999 <yongbinliang@umass.edu>
2016-03-06 15:04:56 -0500

    fix bug with empty query resp status 413

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -4,14 +4,14 @@ class LinesController < ApplicationController
 
 	def get
 		@line = params[:line]
-		@content = NewFile.where(:fileid => 1, :line => @line)[0].content
-
-		puts @content
+		@res_line = NewFile.where(:fileid => 1, :line => @line)[0]
 		
-		if @content
-		      render text: @content, :status => :ok
-		    else
-		      render :file => 'public/413.html', :status => :request_entity_too_large, :layout => false
+		if @res_line		
+			#puts @res_line
+			@content = @res_line.content
+		    render text: @content, :status => :ok
+		else
+		    render :file => 'public/413.html', :status => :request_entity_too_large, :layout => false
 		end
 
 	end
