e215e777bcbfa9b88d45eeee12977f7e6cca0e41
yongbin999 <yongbinliang@umass.edu>
2016-03-06 02:30:46 -0500

    update views

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  resources :new_files
-  resources :new_files
-  resources :new_files
-  resources :new_files
+  
+  	## give all access to new_file
+  	resources :new_files
 
-  	match 'new_file' => 'new_files#index', via: [:get,:post]
-  	match 'newfile/show/:id' => 'new_files#show', via: [:get,:post]
+  	## respect to only these paths as public
+  	match 'new_files' => 'new_files#index', via: [:get]
+  	match 'new_files/new' => 'new_files#new', via: [:get]
+  	match 'new_files/create' => 'new_files#create', via: [:post]
+  	match 'new_files/show' => 'new_files#show', via: [:get]
+
   	match 'lines/:line' => 'lines#get', via: [:get]
 
   	root 'lines#index'
